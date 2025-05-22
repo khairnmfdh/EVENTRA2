3ae0aa59fd13c9b941a5421f69f993881e5b0110
khairnmfdh <khairnmfdh@student.ub.ac.id>
2025-05-22 22:18:56 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.eventra1"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.eventra1"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures { viewBinding = true }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.exifinterface)
+    implementation(libs.play.services.location)
+    implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.room.runtime.android)
+    implementation ("com.karumi:dexter:6.2.3")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.8")
+    implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/eventra1/HistoryViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.eventra1.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.eventra1.database.DatabaseClient.Companion.getInstance
+import com.example.eventra1.database.dao.DatabaseDao
+import com.example.eventra1.model.ModelDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class HistoryViewModel(application: Application) : AndroidViewModel(application) {
+    var dataLaporan: LiveData<List<ModelDatabase>>
+    var databaseDao: DatabaseDao? = getInstance(application)?.appDatabase?.databaseDao()
+
+    fun deleteDataById(uid: Int) {
+        Completable.fromAction {
+            databaseDao?.deleteHistoryById(uid)
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    init {
+        dataLaporan = databaseDao!!.getAllHistory()
+    }
+}
